e63684d3d245268961fe5f10778a12c7c7e40be0
nongrata2 <derbesteindiewelt@mail.ru>
2025-05-02 13:55:32 +0300

    feat: moved config to config.yaml, setting up easy lb

--- cmd/mock/main.go
+++ cmd/mock/main.go
@@ -4,21 +4,19 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-
-	// "net/http/httputil"
-	// "net/url"
-	// "sync"
 	"time"
 )
 
 func startMockServer(port string, delay time.Duration) {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		time.Sleep(delay)
 		fmt.Fprintf(w, "Response from Server %s (delay: %v)", port, delay)
 	})
+
 	log.Printf("Mock server started on :%s (delay: %v)", port, delay)
 	go func() {
-		if err := http.ListenAndServe(":"+port, nil); err != nil {
+		if err := http.ListenAndServe(":"+port, mux); err != nil {
 			log.Fatalf("Failed to start mock server on :%s: %v", port, err)
 		}
 	}()
@@ -28,4 +26,5 @@ func main() {
 	startMockServer("8081", 1*time.Second)
 	startMockServer("8082", 2*time.Second)
 	startMockServer("8083", 3*time.Second)
+	select {}
 }
